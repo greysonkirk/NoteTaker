e86ec836523813bfb04c72ded12fdbc1d3fbdd16
Greyson Kirk <greysonkirk@yahoo.com>
2020-11-30 20:41:49 -0600

    i dont even know

---
+++ server.js
@@ -0,0 +1,18 @@
+ var express = require("express");
+ var fs = require("fs");
+ var path = require("path")
+
+ var app = express();
+ var PORT = process.env.PORT || 3050;
+
+ app.use(express.urlencoded({ extended: true }));
+ app.use(express.json());
+ app.use(express.static(__dirname));
+
+
+ require("./routes/requests")(app);
+
+
+ app.listen(PORT, function() {
+     console.log("App listening on PORT: " + PORT);
+ });
