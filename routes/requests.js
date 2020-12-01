01e14bffbf05629c53e93ddc8b8467e5209349f2
Greyson Kirk <greysonkirk@yahoo.com>
2020-11-30 20:58:21 -0600

    getting somewhere

--- routes/requests.js
+++ routes/requests.js
@@ -36,7 +36,7 @@ module.exports = app => {
             res.sendFile(path.join(__dirname, "../public/notes.html"));
         });
 
-        app.get('', function(req, res) {
+        app.get('*', function(req, res) {
             res.sendFile(path.join(__dirname, "../public/index.html"));
         });
 
